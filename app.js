eea41f4f3a2b5b104ed14b583db751e5d39d54e2
Mouni1905 <103523832+Mouni1905@users.noreply.github.com>
2026-05-05 10:45:49 +0530

    Update response message to include version 3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.end(" Version 2 deployed 🔥");
+  res.end(" Version 3 deployed successfully 🔥");
 });
 
 server.listen(3000);
