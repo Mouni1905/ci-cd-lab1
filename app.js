761060b5f6a113fc7bdd6c0a776e99fb766e0240
Mouni1905 <103523832+Mouni1905@users.noreply.github.com>
2026-05-05 10:49:50 +0530

    Update success message in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
 
-  res.end(" Version 3 deployed successfully 🔥");
+  res.end(" Version 3 deployed successfully without any error🔥");
 });
 
 server.listen(3000);
