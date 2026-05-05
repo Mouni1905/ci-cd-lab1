948900649d2dcd17e6039dd24eae4126463b8a80
Mouni1905 <103523832+Mouni1905@users.noreply.github.com>
2026-05-05 10:37:37 +0530

    Modify response message in server setup
    Updated response message to indicate successful CI/CD setup.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.end("CI/CD Setup Working 🚀");
+  res.end("CI/CD Setup Working successfully 🚀");
 });
 
 server.listen(3000);
